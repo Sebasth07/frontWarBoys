e9113c4178fbd05f8e9f347736d58ae97182e8e9
Sebasth07 <sebasth0709@gmail.com>
2022-03-13 19:16:11 -0500

    Repositorio Inicializado

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// Parallax de los elementos
+	
+	let logo = document.getElementById('logo');
+	let shape1 = document.getElementById('shape1');
+	let shape2 = document.getElementById('shape2');
+	let shape3 = document.getElementById('shape3');
+	let shape4 = document.getElementById('shape4');
+	let shape5 = document.getElementById('shape5');
+	let shape6 = document.getElementById('shape6');
+	let shape7 = document.getElementById('shape7');
+	let shape8 = document.getElementById('shape8');
+	let shape9 = document.getElementById('shape9');
+	let shape10 = document.getElementById('shape10');
+	let shape11 = document.getElementById('shape11');
+	let shape12 = document.getElementById('shape12');
+	let shape13 = document.getElementById('shape13');
+	let shape14 = document.getElementById('shape14');
+	let shape15 = document.getElementById('shape15');
+	let war1 = document.getElementById('war1');
+	let war2 = document.getElementById('war2');
+
+
+window.addEventListener('scroll', function() {
+	var value = window.scrollY;
+
+	logo.style.bottom = value * - 1 + 'px';
+	shape2.style.left = value * - 0.5 + 'px';
+	shape3.style.bottom = value * - 0.1 + 'px';
+	shape4.style.bottom = value * - 0.5 + 'px';
+	shape5.style.bottom = value * - 0.2 + 'px';
+	shape6.style.bottom = value * - 0.5 + 'px';
+	shape7.style.bottom = value * - 0.2 + 'px';
+	shape8.style.left = value * + 0.5 + 'px';
+	shape9.style.left = value * - 0.5 + 'px';
+	shape12.style.left = value * - 1 + 'px';
+	war2.style.right = value * + 0.1 + 'px';
+	war1.style.right = value * - 0.1 + 'px';
+
+
+})
